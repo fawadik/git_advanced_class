5306dc00115f3bf16345d9c3cdedd3ec874f10b3
fawadik <fawadik@yahoo.com>
2019-09-01 15:08:52 +0200

    Good morning function added to the file

--- helloWorld.js
+++ helloWorld.js
@@ -2,6 +2,11 @@ const helloWorld = function() {
   return 'Hello world!';
 };
 
+const goodMorning = function() {
+  return 'Good Morning!';
+};
+
 console.log(helloWorld());
+console.log(goodMorning());
 
 console.log("Hello again");
